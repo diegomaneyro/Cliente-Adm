44589c86f15eda72af264f2ad1c171bc1cefca7d
diegomaneyro <diegomaneyro@gmail.com>
2024-10-14 19:56:36 -0300

    En la rama master  Cambios a ser confirmados: 	modificados:     models/cursor_del_pool.py
    En la llamada al metodo liberarConexion():
    	dentro de la clase cursor_del_pool modifique la llmada de
    devolverConexion a liberrarConexion que provocaba un bug en usuarioDAO

--- models/cursor_del_pool.py
+++ models/cursor_del_pool.py
@@ -12,18 +12,19 @@ class CursorDelPool:
             self._cursor = self._conn.cursor()
             return self._cursor
         except Exception as e:
-            log.debug(f'Ocurrio un error al obtener el cursor')
+            log.debug(f'Ocurrio un error al obtener el cursor: {e}')
             raise e
-        
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         try:
-            if exc_tb or exc_type or exc_tb:
-                log.debug(f'Ocurrio un eror en la ejecucion {exc_val} ')
+            if exc_type or exc_val or exc_tb:
+                log.debug(f'Ocurrio un error en la ejecucion: {exc_val}')
                 self._conn.rollback()
             else:
                 self._conn.commit()
-        finally:    
-            self._cursor.close()            
-            from models.conexion import Conexion            
-            Conexion.devolverConexion(self._conn)
+        finally:
+            self._cursor.close()
+            from models.conexion import Conexion
+            Conexion.liberarConexion(self._conn)
+
             
