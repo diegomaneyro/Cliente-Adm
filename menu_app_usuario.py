a8a075b7442522d86f8676e77b89e246dcbe3bfc
diegomaneyro <diegomaneyro@gmail.com>
2024-10-19 00:41:58 -0300

    En la rama master  Cambios a ser confirmados: 	modificados:     menu_app_usuario.py 	modificados:     models/usuario.py
    en menu_app_usuario:
    	modifico el flujo para que se ejecute con funciones

--- menu_app_usuario.py
+++ menu_app_usuario.py
@@ -1,53 +1,72 @@
-'''
-Menu de opciones del programa paara administrar CRUD 
-en la base de datos usuarios
-'''
 
+"""
+Menu de opciones del programa para administrar CRUD en la base de datos usuarios
+"""
 import sys
 from config.logger_base import log
 from models.conexion import Conexion
 from dao.usuario_dao import UsuarioDAO
 from models.usuario import Usuario
 
+def mostrar_menu():
+    print('''
+    Menú de Opciones:
+    1) Listar usuarios
+    2) Agregar usuario
+    3) Actualizar usuario
+    4) Eliminar usuario
+    5) Salir
+    ''')
 
-salir = False
-while salir is not None:
-    print('''Menu de Opciones
-        Ingrese una opcion (1-5)
-        1) Listar usuarios
-        2) Agregar usuario
-        3) Actualizar usuario
-        4) Eliminar usuario
-        5) Salir''')
-    opcion = int(input('\nElija una opcion para continuar: '))
-    if opcion != 5:
+def listar_usuarios():
+    usuarios = UsuarioDAO.seleccionar()
+    for usuario in usuarios:
+        print(f'\nID_usuario: {usuario.id_usuario} - username: {usuario.username} - password: {usuario.password}')
+
+def agregar_usuario():
+    username = input('Ingrese username: ')
+    password = input('Ingrese password: ')
+    usuario = Usuario(username=username, password=password)
+    UsuarioDAO.insertar(usuario)
+    log.info(f'Usuario agregado: {usuario}')
+
+def actualizar_usuario():
+    id_usuario = int(input('Ingrese ID del usuario a actualizar: '))
+    username = input('Ingrese nuevo username: ')
+    password = input('Ingrese nuevo password: ')
+    usuario = Usuario(id_usuario=id_usuario, username=username, password=password)
+    UsuarioDAO.actualizar(usuario)
+    log.info(f'Usuario actualizado: {usuario}')
+
+def eliminar_usuario():
+    id_usuario = int(input('Ingrese ID del usuario a eliminar: '))
+    usuario = Usuario(id_usuario=id_usuario)
+    UsuarioDAO.eliminar(usuario)
+    log.info(f'Usuario eliminado: {usuario}')
+
+def salir_programa():
+    log.info('Saliendo del programa')
+    Conexion.cerrarConexiones()
+    sys.exit()
+
+if __name__ == '__main__':
+    while True:
+        mostrar_menu()
+        try:
+            opcion = int(input('\nElija una opción para continuar: '))
+        except ValueError:
+            print('Por favor, ingrese un número válido')
+            continue
         
         if opcion == 1:
-            usuarios = UsuarioDAO.seleccionar()
-            for usuario in usuarios:
-                print(f'\nID_usuario: {usuario.id_usuario} -  username: {usuario.username} - password: {usuario.password}')
-        
+            listar_usuarios()
         elif opcion == 2:
-            username = input('Ingrese username: ')
-            password = input('Ingrese password: ')
-            usuario = Usuario(username = username, password = password)
-            UsuarioDAO.insertar(usuario)
-                    
+            agregar_usuario()
         elif opcion == 3:
-            id = int(input('Ingrese id_usuario a actualizar: '))
-            username = input('Ingrese username: ')
-            password = input('Ingrese password: ')            
-            usuario = Usuario(username = username, password = password, id_usuario = id)
-            UsuarioDAO.actualizar(usuario)
-            
+            actualizar_usuario()
         elif opcion == 4:
-            id = int(input('\nIngrese id_usuario para eliminar: '))
-            usuario = Usuario(id_usuario = id)
-            UsuarioDAO.eliminar(usuario)
+            eliminar_usuario()
+        elif opcion == 5:
+            salir_programa()
         else:
-            print('La opcion ingresada no es valida')
-    else:
-        log.info('Saliendo del programa')
-        Conexion.cerrarConexiones()    
-        sys.exit()    
-    
+            print('La opción ingresada no es válida')
--- models/usuario.py
+++ models/usuario.py
@@ -1,8 +1,32 @@
 class Usuario:    
+    """
+    Clase para representar un usuario.
+
+    Atributos:
+    -----------
+    id_usuario : int
+        ID del usuario.
+    username : str
+        Nombre de usuario.
+    password : str
+        Contraseña del usuario.
+    """
     def __init__(self,id_usuario= None, username = None, password = None):
         self._id_usuario = id_usuario
         self._username = username
         self._password = password
+    """
+        Inicializa un nuevo usuario.
+
+        Parámetros:
+        -----------
+        id_usuario : int, opcional
+            ID del usuario (por defecto None).
+        username : str, opcional
+            Nombre de usuario (por defecto None).
+        password : str, opcional
+            Contraseña del usuario (por defecto None).
+    """
 
     def __str__(self):
         return f'Id_usuario: {self._id_usuario}, username: {self._username}, password: {self._password}'
