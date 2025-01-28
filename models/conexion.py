07ee92e8c0392262ea17e472e24b8bf574c6dc80
diego maneyro <diegomaneyro@gmail.com>
2025-01-27 23:02:05 -0300

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   .env
    	modified:   models/conexion.py
    	modified:   requirements.txt
    	modified:   tests/test_conexion.py

--- models/conexion.py
+++ models/conexion.py
@@ -1,53 +1,51 @@
-from psycopg2 import pool
+from psycopg_pool import ConnectionPool
 from config.logger_base import log
-from models.cursor_del_pool import CursorDelPool
 import sys
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Conexion:
 
-    _DATABASE = 'persona'
-    _USERNAME = 'postgres'
-    _PASSWORD = 'admin'
-    _DB_PORT = '5432'
-    _HOST = '127.0.0.1'
+    _DATABASE = os.getenv('DB_NAME')
+    _USERNAME = os.getenv('DB_USER')
+    _PASSWORD = os.getenv('DB_PASSWORD')
+    _DB_PORT = os.getenv('DB_PORT')
+    _HOST = os.getenv('DB_HOST')
     _MIN_CON = 1
     _MAX_CON = 6
     _pool = None
-
 
     @classmethod
     def obtenerPool(cls):
         if cls._pool is None:
             try:
-                cls._pool = pool.SimpleConnectionPool(cls._MIN_CON, cls._MAX_CON,
-                                                host = cls._HOST,
-                                                user = cls._USERNAME,
-                                                port = cls._DB_PORT,
-                                                database = cls._DATABASE,
-                                                password = cls._PASSWORD
-                                                )
-                log.debug(f'Se creo el pool de conexiones correctamente')
+                conn_str = f"postgresql://{cls._USERNAME}:{cls._PASSWORD}@{cls._HOST}:{cls._DB_PORT}/{cls._DATABASE}"
+                cls._pool = ConnectionPool(conninfo=conn_str, min_size=cls._MIN_CON, max_size=cls._MAX_CON)                
+                log.debug(f'Se creó el pool de conexiones correctamente')
                 return cls._pool
             except Exception as e:
-                log.error(f'Ocurrio un error al crear el pool de conexiones {e}')
+                log.error(f'Ocurrió un error al crear el pool de conexiones: {e}')
                 sys.exit()
         else:
             return cls._pool
 
     @classmethod
-    def obtenerConexion(cls):       
-        if cls._pool == None:
+    def obtenerConexion(cls):
+        if cls._pool is None:
             cls.obtenerPool()
         return cls._pool.getconn()
-            
+
     @classmethod
     def liberarConexion(cls, conexion):
-        cls.obtenerPool().putconn(conexion)                   
+        if cls._pool is not None:
+            cls._pool.putconn(conexion)
 
     @classmethod
     def cerrarConexiones(cls):
         if cls._pool is not None:
-            cls._pool.closeall()
+            cls._pool.close()
             log.debug('Todas las conexiones del pool han sido cerradas')
         else:
             log.debug('No hay conexiones en el pool para cerrar')
--- tests/test_conexion.py
+++ tests/test_conexion.py
@@ -1,18 +1,18 @@
 import unittest
-from models.conexion import Conexion
+from models.conexion import *
 from config.logger_base import log
+from psycopg import OperationalError
 
 '''
-setUpClass y tearDownClass: Métodos para ejecutar acciones antes y después de todas las pruebas, 
+setUpClass y tearDownClass: Métodos para ejecutar acciones antes y después de todas las pruebas,
 ideal para inicializar y finalizar el logging.
 
-setUp y tearDown: Métodos que se ejecutan antes y después de cada prueba individual, 
+setUp y tearDown: Métodos que se ejecutan antes y después de cada prueba individual,
 útiles para registrar cada inicio y fin de prueba.
 
 Uso de log.info en las pruebas: Para registrar información sobre el estado y el progreso
 de las pruebas en los logs, proporcionando detalles adicionales.
 '''
-
 
 class TestConexion(unittest.TestCase):
 
@@ -23,6 +23,7 @@ class TestConexion(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         log.info("Finalizando las pruebas de la clase Conexion")
+        Conexion.cerrarConexiones()  # Asegurarse de cerrar todas las conexiones al finalizar todas las pruebas
 
     def setUp(self):
         log.info("Iniciando una prueba...")
@@ -31,21 +32,30 @@ class TestConexion(unittest.TestCase):
         log.info("Prueba finalizada.")
 
     def test_obtenerPool(self):
-        pool = Conexion.obtenerPool()
-        log.info(f"Pool obtenido: {pool}")
-        self.assertIsNotNone(pool, "El pool de conexiones no debería ser None si se creó correctamente")
+        try:
+            pool = Conexion.obtenerPool()
+            log.info(f"Pool obtenido: {pool}")
+            self.assertIsNotNone(pool, "El pool de conexiones no debería ser None si se creó correctamente")
+        except OperationalError as e:
+            self.fail(f"Error al obtener el pool de conexiones: {e}")
 
     def test_obtenerConexion(self):
-        conexion = Conexion.obtenerConexion()
-        log.info(f"Conexión obtenida: {conexion}")
-        self.assertIsNotNone(conexion)
-        Conexion.liberarConexion(conexion)
+        try:
+            conexion = Conexion.obtenerConexion()
+            log.info(f"Conexión obtenida: {conexion}")
+            self.assertIsNotNone(conexion)
+            Conexion.liberarConexion(conexion)
+        except OperationalError as e:
+            self.fail(f"Error al obtener la conexión: {e}")
 
     def test_liberarConexion(self):
-        conexion = Conexion.obtenerConexion()
-        log.info(f"Liberando conexión: {conexion}")
-        Conexion.liberarConexion(conexion)
-        self.assertFalse(conexion.closed)  # Verifica que la conexión no esté cerrada
+        try:
+            conexion = Conexion.obtenerConexion()
+            log.info(f"Liberando conexión: {conexion}")
+            Conexion.liberarConexion(conexion)
+            self.assertFalse(conexion.closed)  # Verifica que la conexión no esté cerrada
+        except OperationalError as e:
+            self.fail(f"Error al liberar la conexión: {e}")
 
 if __name__ == '__main__':
     unittest.main()
